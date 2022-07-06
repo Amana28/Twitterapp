dab15b5da2ca16b8946776ae2a0ebab15bda8a4e
Amana28 <76058143+Amana28@users.noreply.github.com>
2022-07-06 12:13:08 -0400

    Added some functions

--- twitterapp.py
+++ twitterapp.py
@@ -1,6 +1,34 @@
 from pytwitter import Api
+import json
 
 api = Api(bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFNSeQEAAAAAayPWo8IHonqMSkMAZxDBemHu3JA%3D5yaETCA3pDYOSjepwVGxKayvgy1VIwUPqOWaz1HSAJ2YZBwtFQ')
-id = api.get_users(ids=["783214", "2244994945"])
 
-print(id)
+
+def get_username(id):
+    response = api.get_user(user_id = id)
+    return response.data.username
+
+
+def get_following(id):
+    response = api.get_following(user_id = id, max_results = 100)
+    return response.data
+
+
+def get_followers(id):
+    response = api.get_followers(user_id = id, max_results = 100)
+    return response.data
+
+
+def main():
+    following = get_following("1542616796888309760")
+    for account in following:
+        print(account.name)
+
+    followers = get_followers("1542616796888309760")
+    for follower in followers:
+        print(follower.username)
+
+   
+
+if __name__ == "__main__":
+    main()
